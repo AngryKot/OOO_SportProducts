e030816eb83f36c599db1450e6fd2c8acaf74a6d
AngryKot <segeda.nikita2016@yandex.ru>
2023-10-19 13:05:40 +0300

    Добавьте файлы проекта.

---
+++ OOO_SportProducts/Classes/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOO_SportProduct.Classes
+{
+    internal class Helper
+    {
+         public static OOO_SportProducts.Model.DBsport403Entities dBSportEntities { get; set; } //доступное свойство связи с бд
+    }
+}
